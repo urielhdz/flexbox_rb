cc25c31690f9fa0bc2f3fa88df7f1a63820a4dd0
urielhdz <uriel.hdzc@gmail.com>
2015-03-24 12:41:02 -0600

    Fixed xs,md,lg

--- lib/flexbox_rb/my_flexbox_helper.rb
+++ lib/flexbox_rb/my_flexbox_helper.rb
@@ -1,6 +1,6 @@
 module MyFlexboxHelper
 	
-	["md,xs,lg"].each do |device|
+	["md","xs","lg","sm"].each do |device|
 		1..13.times do |val|
 			define_method("grid_#{device}_#{val}") do |args = {}, &block|
 				args[:class] = args.has_key :class ? "#{args[:class]} col-#{device}-#{val}" : "col-#{device}-#{val}"
