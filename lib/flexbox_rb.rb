fbe327d26c76826190d7863ddcc6b379ed344aa3
urielhdz <uriel.hdzc@gmail.com>
2015-03-24 12:33:56 -0600

    Fixed helper

--- lib/flexbox_rb.rb
+++ lib/flexbox_rb.rb
@@ -5,7 +5,7 @@ module FlexboxRb
   class Engine < ::Rails::Engine
   	initializer "my_gem.include_view_helpers" do |app|
       ActiveSupport.on_load :action_view do
-        include MyFlexBoxHelper
+        include MyFlexboxHelper
       end
     end
   end
