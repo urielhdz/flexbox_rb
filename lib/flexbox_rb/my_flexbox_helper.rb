a805154dd3c175139b06c585cccaf46200fe5b13
urielhdz <uriel.hdzc@gmail.com>
2015-04-08 13:24:20 -0500

    fix #1 added grid method

--- lib/flexbox_rb/my_flexbox_helper.rb
+++ lib/flexbox_rb/my_flexbox_helper.rb
@@ -4,12 +4,24 @@ module MyFlexboxHelper
 			define_method("grid_#{device}_#{val}") do |args = {}, &block|
 				args[:class] = "#{args[:class]} col-#{device}-#{val}"
 				attrs = args.map {|k,v| "#{k}='#{v}' " }.join(" ")
-				html= "<div #{attrs}>" \
-							"<div class='box'>" \
-							"#{capture(&block)}" \
-							"</div></div>"
-				html.html_safe
+				html_generator attrs, block 
 			end
 		end
+	end
+	def html_generator attrs_string, block
+		html= "<div #{attrs_string}>" \
+					"<div class='box'>" \
+					"#{capture(&block) unless block.nil?}" \
+					"</div></div>"
+		html.html_safe
+	end
+	def grid args = {},&block
+		[:xs,:sm,:md,:lg].each do |device|
+			if args.has_key? device
+				args[:class] =  "#{args[:class]} col-#{device}-#{args[device]}"
+			end
+		end
+		attrs = args.except!(:md,:lg,:xs,:sm).map {|k,v| "#{k}='#{v}' " }.join(" ")
+		html_generator attrs, block
 	end
 end
--- lib/flexbox_rb/version.rb
+++ lib/flexbox_rb/version.rb
@@ -1,3 +1,3 @@
 module FlexboxRb
-  VERSION = "0.2.7.4"
+  VERSION = "0.2.8"
 end
